d56f194302c956ccedb264b7bd61b3335f716061
Felo <felipemelo713@gmail.com>
2023-06-21 13:10:45 -0300

    adicionado Spinner, tratamento de erros e pipe para mostrar icones

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,10 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+
 import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -9,16 +13,26 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [  ];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
     // this.courses = [];
-    this.courses = this.coursesService.list()
+    this.courses$ = this.coursesService.list().pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.')
+        return of ([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void {
-
   }
 
 }
