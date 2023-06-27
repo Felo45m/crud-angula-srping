20fe5ad0567a29b651ec21b28a709ef352d1b226
Felo <felipemelo713@gmail.com>
2023-06-27 15:03:10 -0300

    Criado metodo para adicionar curso ao formulario na API Spring através do HTTP Post

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -13,7 +14,12 @@ export class CourseFormComponent {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private service: CoursesService, private _snackBar: MatSnackBar) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location
+    ) {
     this.form = formBuilder.group({
       name: [null],
       category: [null]
@@ -21,16 +27,20 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
-    this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error => this.onError())
+    this.service.save(this.form.value).subscribe(result => this.onSuccess(), error => this.onError())
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration : 3000 });
+    this.onCancel();
   }
 
   private onError() {
-    this._snackBar.open('Erro ao salvar curso.', '', { duration : 5000 });
+    this.snackBar.open('Erro ao salvar curso.', '', { duration : 5000 });
   }
 
 }
--- crud-spring/src/main/java/com/felo/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/felo/crudspring/CrudSpringApplication.java
@@ -1,12 +1,7 @@
 package com.felo.crudspring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.felo.crudspring.model.Course;
-import com.felo.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -15,16 +10,4 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDataBase(CourseRepository courseRepository) {
-		return args -> {
-			courseRepository.deleteAll();
-			
-			Course c = new Course();
-			c.setName("Angular + spring");
-			c.setCategory("front-end");
-			
-			courseRepository.save(c);
-		};
-	}
 }
