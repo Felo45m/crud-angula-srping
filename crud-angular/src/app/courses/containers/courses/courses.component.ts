58e046507b53533d5c2d24bc071d17720aaf4695
Felo <felipemelo713@gmail.com>
2023-07-21 11:48:07 -0300

    Adicionado metodo HTTP DELETE no back-end e no front-end

--- crud-angular/src/app/courses/containers/courses/courses.component.ts
+++ crud-angular/src/app/courses/containers/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/error-dialog/confirmation-dialog/confirmation-dialog.component';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from '../../model/course';
@@ -14,14 +16,19 @@ import { CoursesService } from '../../services/courses.service';
 })
 export class CoursesComponent {
 
-  courses$: Observable<Course[]>;
+  courses$: Observable<Course[]> | null = null;
 
   constructor(
     private coursesService: CoursesService,
     public dialog: MatDialog,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
     ) {
+    this.refresh();
+  }
+
+  refresh() {
     this.courses$ = this.coursesService.list()
       .pipe(
         catchError(error => {
@@ -43,5 +50,28 @@ export class CoursesComponent {
 
   onEdit(course: Course) {
     this.router.navigate(['edit', course._id], {relativeTo: this.route})
+  }
+
+  onDelete(course: Course) {
+
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: 'Tem certeza que deseja remover esse curso?',
+    });
+
+    dialogRef.afterClosed().subscribe( ( result: boolean ) => {
+      if (result) {
+        this.coursesService.delete(course._id).subscribe(
+          () => {
+            this.refresh();
+            this.snackBar.open('Curso removido com sucesso!', 'X', {
+              duration : 5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center'
+            });
+          },
+          error => this.onError('Erro ao tentar remover curso.')
+        );
+      }
+    });
   }
 }
